f660b454967b55edc466cb80d23458b9013797ba
Beedrill Poetess <bpoetess@oa-d1.msk.21-school.ru>
2022-04-06 17:36:15 +0300

    a little bugfix

--- fdf.c
+++ fdf.c
@@ -6,7 +6,7 @@
 /*   By: bpoetess <bpoetess@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/30 01:13:08 by bpoetess          #+#    #+#             */
-/*   Updated: 2022/04/06 17:11:47 by bpoetess         ###   ########.fr       */
+/*   Updated: 2022/04/06 17:35:26 by bpoetess         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,6 +52,21 @@ void	fdf_setdots(t_glb *glb, int x, int y, int even)
 	glb->buf[4 + even] = glb->mtx[y][2 * x + 1];
 }
 
+void	fdf_draw_set(t_glb *glb, int x, int y)
+{
+	fdf_setdots(glb, x, y, 0);
+	if (x < glb->x - 1)
+	{
+		fdf_setdots(glb, x + 1, y, 1);
+		fdf_bresenham(glb);
+	}
+	if (y < glb->y - 1)
+	{
+		fdf_setdots(glb, x, y + 1, 1);
+		fdf_bresenham(glb);
+	}
+}
+
 void	fdf_draw(t_glb *glb)
 {
 	int	x;
@@ -60,16 +75,12 @@ void	fdf_draw(t_glb *glb)
 	if (!glb)
 		return ;
 	x = 0;
-	while (x < glb->x - 1)
+	while (x < glb->x)
 	{
 		y = 0;
-		while (y < glb->y - 1)
+		while (y < glb->y)
 		{
-			fdf_setdots(glb, x, y, 0);
-			fdf_setdots(glb, x + 1, y, 1);
-			fdf_bresenham(glb);
-			fdf_setdots(glb, x, y + 1, 1);
-			fdf_bresenham(glb);
+			fdf_draw_set(glb, x, y);
 			y++;
 		}
 		x++;
